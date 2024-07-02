9401727c8b9f128169b833c7c6761fd7108640fa
Lara369 <prabhakarlara0706@gmail.com>
2024-07-02 13:01:15 +0530

    Added circuit-breaker in product service

--- src/main/java/com/targetindia/EcomStreaming/service/ProductServiceImpl.java
+++ src/main/java/com/targetindia/EcomStreaming/service/ProductServiceImpl.java
@@ -3,6 +3,8 @@ package com.targetindia.EcomStreaming.service;
 import com.targetindia.EcomStreaming.entites.Products;
 import com.targetindia.EcomStreaming.exceptions.ProductNotFoundException;
 import com.targetindia.EcomStreaming.repository.ProductRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -37,7 +39,15 @@ public class ProductServiceImpl implements ProductService{
     }
 
     @Override
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackDisplayAllProducts")
     public List<Products> displayAllProducts(){
-        return productRepository.findAll();
+        throw new RuntimeException("Simulated service failure");
+        //return productRepository.findAll();
+    }
+
+    public List<Products> fallbackDisplayAllProducts(Throwable throwable) {
+        // Fallback logic if displayAllProducts fails
+        System.out.println("Fallback called for displayAllProducts due to: " + throwable.getMessage());
+        return List.of(); // or return an empty list or handle accordingly
     }
 }
